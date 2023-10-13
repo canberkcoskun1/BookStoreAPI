2c6e5aaddef528162d5e57bf2f0a0f696025b745
canberkcoskun1 <canberkcoskun57@gmail.com>
2023-10-13 11:11:42 +0300

    Add project files.

---
+++ BookStore.Core/Common/IDeletable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Common
+{
+    public interface IDeletable
+    {
+        bool IsDeleted { get; set; }
+        DateTime? DeletedAt { get; set; }
+
+    }
+}
